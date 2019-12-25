af3cad8490ff97c4fbe65ddb516f13bce6e808bc
Nikelson Exantus <nikelsone1981@hotmail.com>
2019-12-25 07:58:04 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+ 
+#  def initialize(name)
+#   @name = dog_name 
+# end 
+ 
+  # Read the name of the dog from an instance variable
+  def name 
+    @name 
+  end
+  
+  def name=(dog_name)
+  end 
+  
+  
+end
+    
