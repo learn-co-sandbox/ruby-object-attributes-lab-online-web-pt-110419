d585696cfa899fa6eb370a6dd0888c25a66ffc92
Nikelson Exantus <nikelsone1981@hotmail.com>
2019-12-25 09:25:57 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,14 +2,14 @@ class Person
   def initialize(first_name, last_name, job)
     @first_name = first_name
 	  @last_name = last_name
-	  @job = job
+	  @job = job_description
   end 
   
-  def name=(full_name, job)
+  def name=(full_name)
     @first_name, @last_name = full_name.split
     @first_name = first_name
     @last_name = last_name
-    @job = job_description 
+     
   end
   
   def name
