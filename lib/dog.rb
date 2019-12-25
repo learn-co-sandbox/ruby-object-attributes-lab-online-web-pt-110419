47cdf07fc959ab7248afa594bc9ac928daf2e79c
Nikelson Exantus <nikelsone1981@hotmail.com>
2019-12-25 09:28:51 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,10 +6,11 @@ class Dog
  
   # Read the name of the dog from an instance variable
   def name 
-    @name 
+    @this_dog_name
   end
   
   def name=(dog_name)
+    @this_dog_name = dog_name
   end 
   
   
